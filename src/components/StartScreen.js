10bca04515b14a7971d6201b657dca9d469a3f44
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-05-01 13:18:42 +0100

    cleaned up project and added components and its css

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Click in the button to start the game</p>
+      <button>Start Game</button>
+    </div>
+  );
+};
+
+export default StartScreen;
