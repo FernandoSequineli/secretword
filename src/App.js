9f1833ad029fe76e73559935f4f1aa791be9dddc
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-05-02 18:08:14 +0100

    crated connection between the pages, added function to pick letter and category and added html elements to Game component

--- src/App.js
+++ src/App.js
@@ -6,21 +6,56 @@ import StartScreen from "./components/StartScreen";
 import Game from "./components/Game";
 import GameOver from "./components/GameOver";
 
-const Stages = [
+const stages = [
   { id: 1, name: "start" },
   { id: 2, name: "game" },
   { id: 3, name: "end" },
 ];
 
 function App() {
-  const [gameStage, setGameStage] = useState(Stages[0].name);
+  const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
-  console.log(words);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  };
+
+  const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
+    setGameStage(stages[1].name);
+  };
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,33 @@
 import "./Game.css";
 
-const Game = () => {
-  return <div>Game</div>;
+const Game = ({ verifyLetter }) => {
+  return (
+    <div className="game">
+      <p className="points">
+        <span>Score: 0</span>
+      </p>
+      <h1>Guess the word</h1>
+      <h3 className="tip">
+        Tip: <span>Tip...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Try to guess a letter:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Play!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letters already used:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
+    </div>
+  );
 };
 
 export default Game;
